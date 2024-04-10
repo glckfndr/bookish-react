5ab9a098bedd8c0866d97035a0f925200796f598
glckfndr <obulan65@gmail.com>
2024-04-10 19:29:25 +0300

    Initial commit with Material UI Library

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Typography from "@mui/material/Typography";
+
+function App() {
+  return (
+    <div>
+      <Typography variant="h2" component="h2" data-test="heading">
+        Book Store
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
